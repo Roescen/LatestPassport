1ce7429665a707749efe44738e95a48ed9250638
RoescenAbiePajaro <114593776+Roescen@users.noreply.github.com>
2025-05-19 11:03:30 +0800

    eto lang

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -8,7 +8,6 @@ import DashComments from '../components/DashComments';
 import DashboardComp from '../components/DashboardComp';
 import DashCategories from '../components/DashCategories'; // Updated import
 import UpdateCategory from './UpdateCategory';
-import CreatePost from '../components/CreatePost';
 import UpdatePost from './UpdatePost';
 import LoadingSpinner from '../components/LoadingSpinner';
 import UserApproval from '../components/UserApproval';
@@ -71,8 +70,6 @@ export default function Dashboard() {
         {tab === 'updatecategory' && <UpdateCategory categoryId={categoryId} />}
         {tab === 'userapproval' && <UserApproval />}
         {tab === 'feedback' && <DashFeedback />}
-        {tqb === 'createpost' && <CreatePost />}
-
       </div>
     </div>
   );
